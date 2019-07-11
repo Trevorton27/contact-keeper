ff8b6c617e28a5a9d1bceaa7ee97e37530232fe2
Trevor Mearns <spiral272@gmail.com>
2019-07-11 15:11:37 +0900

    Express server and initial routes setup, all functionality a go, API endpoints responding

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.json({msg: 'Alas we welcome you to yon school contact keeper. Verily that thou wouldst thus for thee and whatnot.'})
+);
+
+app.listen(PORT, () => console.log(`Server done started on port ${PORT}`));
