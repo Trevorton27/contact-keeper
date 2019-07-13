7df967274a327ff79a879e00f846524b1c85b77e
Trevorton27 <spiral272@gmail.com>
2019-07-13 17:22:50 +0900

    database connection now checking and accepting user access credentials

--- models/User.js
+++ models/User.js
@@ -19,4 +19,4 @@ const UserSchema = mongoose.Schema({
     }
 });
 
-module.export = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('user', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
-
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
+const config = require('config');
 //@route POST api/users
 //@desc register a user
 //@access Public
@@ -18,16 +19,45 @@ async (req, res) => {
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+   
     const { name, email, password } = req.body
 
     try {
-        let user = await User.findOne({ email});
+       
+        let user = await User.findOne({ email: email });
 
         if(user) {
           return res.status(400).json({ msg: 'User already exists'})
         }
+
+        user = new User({
+          name,
+          email,
+          password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+       const payload = {
+         user: {
+           id: user.id
+         }
+       }
+
+       jwt.sign(payload, config.get('jwtSecret'), { 
+         expiresIn: 360000
+       }, (err, token) => {
+         if(err) throw err;
+         res.json({token})
+       }
+       );
+
     } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error dawg 残念')
 
     }
 
