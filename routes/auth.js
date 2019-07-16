09c1782c6df21122cf16449f048e7ddd25a42ca1
Trevorton27 <spiral272@gmail.com>
2019-07-16 14:33:21 +0900

    all kinds of crud functions built but have hit roadblock with put request :-(

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,22 @@ const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleWare/auth');
+
 
 const User = require('../models/User');
 //@route GET api/auth
 //@desc GET logged in user
 //@access Private
 
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,28 +1,86 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const auth = require('../middleWare/auth');
+const Contact = require('../models/Contact');
 
 //@route GET api/contacts
 //@desc Get all users contacts
 //@access Private
 
-router.get('/', (req, res) => {
-    res.send('get all contacts');
+router.get('/', auth, async (req, res) => {
+  
+    try {
+        const contacts = await Contact.find({user: req.user.id}). sort({date: -1});
+        res.json(contacts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 //@route POST api/contacts
 //@desc Add new contact
 //@access Private
 
-router.post('/', (req, res) => {
-    res.send('Add new contact');
+router.post('/', [auth, [
+    check('name', 'Name is required').not().isEmpty()
+]] ,async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {name, email, phone, type } = req.body;
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+        res.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send(`Server Error`);
+    }
 });
 
 //@route PUT api/contacts/:id
 //@desc Add new contact
 //@access Private
 
-router.put('/:id', (req, res) => {
-    res.send('Update contact');
+router.put('/:id', auth, async (req, res) => {
+    const {name, email, phone, type } = req.body;
+
+    //build contact object
+    const contactFileds = {};
+    if(name) contactFileds.name = name;
+    if(email) contactFileds.email = email;
+    if(phone) contactFileds.phone = phone;
+    if(type) contactFileds.type = type;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+        if(!contact) return res.status(404).json({msg: 'Apologies. Contact not found'});
+        // Make sure user owns contact
+        if(contact.user.toString !== req.user.id) {
+            return res.status(401).json({ msg: 'You are not authorized to make changes to this user\`s account'});
+        }
+
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+            { $set: contactFields},
+            {new: true });
+
+            res.json(contact);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send(`Server Error`);
+    }
 });
 
 //@route DELETE api/contacts/:id
