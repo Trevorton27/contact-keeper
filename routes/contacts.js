9d54fe58e5916acbb342a4a1b6a625d754d2608c
Trevorton27 <spiral272@gmail.com>
2019-07-16 18:40:54 +0900

    updated .gitignore file

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const User = require('../models/User');
 const auth = require('../middleWare/auth');
 const Contact = require('../models/Contact');
 
@@ -26,7 +25,7 @@ router.get('/', auth, async (req, res) => {
 
 router.post('/', [auth, [
     check('name', 'Name is required').not().isEmpty()
-]] ,async (req, res) => {
+] ], async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -58,17 +57,17 @@ router.put('/:id', auth, async (req, res) => {
     const {name, email, phone, type } = req.body;
 
     //build contact object
-    const contactFileds = {};
-    if(name) contactFileds.name = name;
-    if(email) contactFileds.email = email;
-    if(phone) contactFileds.phone = phone;
-    if(type) contactFileds.type = type;
+    const contactFields = {};
+    if(name) contactFields.name = name;
+    if(email) contactFields.email = email;
+    if(phone) contactFields.phone = phone;
+    if(type) contactFields.type = type;
 
     try {
         let contact = await Contact.findById(req.params.id);
         if(!contact) return res.status(404).json({msg: 'Apologies. Contact not found'});
         // Make sure user owns contact
-        if(contact.user.toString !== req.user.id) {
+        if(contact.user.toString() !== req.user.id) {
             return res.status(401).json({ msg: 'You are not authorized to make changes to this user\`s account'});
         }
 
@@ -87,7 +86,22 @@ router.put('/:id', auth, async (req, res) => {
 //@desc DELETE contact
 //@access Private
 
-router.delete('/:id', (req, res) => {
-    res.send('Delete contact');
+router.delete('/:id', auth, async (req, res) => {
+    
+    try {
+        let contact = await Contact.findById(req.params.id);
+        if(!contact) return res.status(404).json({msg: 'Apologies. Contact not found'});
+        // Make sure user owns contact
+        if(contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'You are not authorized to make changes to this user\`s account'});
+        }
+
+        await Contact.findByIdAndRemove(req.params.id);
+        res.json({msg: 'Contact has been removed.'})
+
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send(`Server Error`);
+    }
 });
 module.exports = router;
