1db49d08a5377d602bdfcb4b42937e47445b31ae
Trevorton27 <spiral272@gmail.com>
2019-07-23 20:11:35 +0900

    changed heroku-post build and build scripts to reconfigure deployment to Heroku

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/users', require('./routes/users'));
 
 //Server static assets in production
-if(process.env.NODE_ENV === 'production') {
+//if(process.env.NODE_ENV === 'production') {
     //set static folder
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html')));
-}
+//}
 
 const PORT = process.env.PORT || 5000;
 
