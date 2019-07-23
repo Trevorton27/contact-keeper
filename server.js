17ec5b9fe91eca30d7033581e00fe310ffbcecec
Trevorton27 <spiral272@gmail.com>
2019-07-23 20:24:40 +0900

    added extra underscore to dirname parameter lines 19 and 20 server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use('/api/users', require('./routes/users'));
 //Server static assets in production
 //if(process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use('/static', express.static(path.join(_dirname, 'client', 'build', 'static')));
-    app.get('*', (req, res) => res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html')));
+    app.use('/static', express.static(path.join(__dirname, 'client', 'build', 'static')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 //}
 
 const PORT = process.env.PORT || 5000;
