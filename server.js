394a7810817f733496441e1267aba2eea80e71bd
Trevorton27 <spiral272@gmail.com>
2019-07-23 20:31:15 +0900

    changed package.json and server.js back to tutorial standard (with longer underscore for dirname paramater)

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 // connect database
 connectDB();
 
+
+
 //Init Middleware
 app.use(express.json({ extended: false}));
 
@@ -14,11 +16,13 @@ app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/users', require('./routes/users'));
 
 //Server static assets in production
-//if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use('/static', express.static(path.join(__dirname, 'client', 'build', 'static')));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-//}
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 
